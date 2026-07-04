c4668f8ac76dc976ce523968e511838983ef108a
Dinesh Sinnathamby <103861462+crackle2k@users.noreply.github.com>
2026-07-04 10:45:02 -0400

    Implement Goal Parser Interpretation solution

---
+++ LeetCode/Python/Easy/1678 - Goal Parser Interpretation.py
@@ -0,0 +1,12 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        final = ""
+        for i in range(len(command)):
+            if command[i] == 'G':
+                final += 'G'
+            elif command[i] == '(':
+                if command[i + 1] == 'a':
+                    final += 'al'
+                elif command[i + 1] == ')':
+                    final += 'o'
+        return final
