357800bae5100047f9ce817bf7f729ec2bf76ea5
Dinesh Sinnathamby <103861462+crackle2k@users.noreply.github.com>
2026-07-06 12:39:57 -0400

    feat: solution to check if sentence is pangram

---
+++ LeetCode/Python/Easy/1832 - Check if the Sentence Is Pangram.py
@@ -0,0 +1,6 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        for letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+            if letter not in sentence:
+               return False
+        return True
